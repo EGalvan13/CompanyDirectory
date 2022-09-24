cfa053d685addfe2f7ae54874f5dc664fcb50e42
EsauG <esau_galvan13@outlook.com>
2022-09-23 20:48:07 -0500

    better practices applied

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,15 +1,15 @@
 <script setup>
-  import MainCardsSingle from './MainCardsSingle.vue'
+  import MainCardsSingle from '@/components/MainCardsSingle.vue'
 </script>
 
 <template>
   <div class="sub-wrapper">
-    <MainCardsSingle v-for="n in 20" />
+    <MainCardsSingle v-for="n in 20" :key="n" />
   </div>
 </template>
 
 <style scoped lang="postcss">
   .sub-wrapper {
-    @apply container mx-auto flex flex-col gap-4 py-4;
+    @apply grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
   }
 </style>
