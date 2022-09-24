40f773da3aaad0859e61399b3ec7f5e6f9bfb3b9
EsauG <esau_galvan13@outlook.com>
2022-09-23 21:43:45 -0500

    added router

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,47 +1,47 @@
 <script setup>
-import { ref } from 'vue'
-const brand = ref('Esaus Fake Company Directory')
+  import { ref } from 'vue'
+  const brand = ref('Esaus Fake Company Directory')
 </script>
 
 <template>
-    <nav>
-        <div class="wrapper">
-            <div class="brand">
-                <span class="brand-title">{{ brand }}</span>
-            </div>
-            <div class="menu">
-                <a href="#" class="menu-item">Departments</a>
-                <a href="#" class="menu-item">Settings</a>
-                <a href="#" class="menu-login">Logout</a>
-            </div>
-        </div>
-    </nav>
+  <nav>
+    <div class="wrapper">
+      <RouterLink :to="{ name: 'Home' }" class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </RouterLink>
+      <div class="menu">
+        <a href="#" class="menu-item">Departments</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-login">Logout</a>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped lang="postcss">
-nav {
+  nav {
     @apply flex h-20 bg-slate-900 text-slate-200;
 
     .wrapper {
-        @apply container mx-auto flex w-full items-center justify-between;
+      @apply container mx-auto flex w-full items-center justify-between;
 
-        .brand {
-            &-title {
-                @apply text-2xl font-bold text-blue-300;
-            }
+      .brand {
+        &-title {
+          @apply text-2xl font-bold text-blue-300;
+        }
+      }
+
+      .menu {
+        @apply flex gap-2;
+
+        &-item {
+          @apply rounded-md px-4 py-2;
         }
 
-        .menu {
-            @apply flex gap-2;
-
-            &-item {
-                @apply rounded-md px-4 py-2;
-            }
-
-            &-login {
-                @apply rounded-md bg-red-500 px-4 py-2 text-red-100;
-            }
+        &-login {
+          @apply rounded-md bg-red-500 px-4 py-2 text-red-100;
         }
+      }
     }
-}
+  }
 </style>
